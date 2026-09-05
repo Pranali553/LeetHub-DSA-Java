397f6d3a48fc486a3ab49caabaaf8becb5cd002d
Pranali Patil <pranalipatil572004@gmail.com>
2026-09-05 15:32:55 +0530

    Time: 483 ms (94.9%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select p.project_id ,
+ROUND(avg(experience_years), 2) AS average_years
+from 
+Project p
+join
+Employee e
+on
+p.employee_id = e.employee_id
+group by
+project_id;
